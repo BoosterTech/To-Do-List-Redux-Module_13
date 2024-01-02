d3ee3f0ab611eeec1793916cea9594b3aae439a3
Dariusz Podczasik <boostertech@mail.com>
2024-01-02 16:05:43 +0000

    Redux store created

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import reportWebVitals from "./App.js";
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
+import store from "./store.js";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App/>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
